d308136114f4ec013509bdda7a9b52b284a3696a
tiago-kelis <tiagokelis2017@gmail.com>
2025-05-28 19:41:02 -0300

    Adicionar makemigrations e migrate ao build

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Instalar dependências
 pip install -r requirements.txt
 
+# Fazer as migrações
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+
+# Coletar arquivos estáticos
 python manage.py collectstatic --no-input
-python manage.py migrate
