45770eead3b6edd73b57e2f03cb27b751fdaf03d
tiago-kelis <tiagokelis2017@gmail.com>
2025-05-29 01:21:10 -0300

    adicionando id no enterprise

--- accounts/views/user.py
+++ accounts/views/user.py
@@ -1,23 +1,26 @@
 from accounts.views.base import Base
 from accounts.models import User
 from accounts.serializers import UserSerializer
-    
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
-    
+from rest_framework import status
+
 class GetUser(Base):
-    permission_classes=[IsAuthenticated]
-    
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        user = User.objects.filter(id=request.user.id).first()
-        enterprise = self.get_enterprise_user(user)
-    
+        try:
+            user = User.objects.get(id=request.user.id)
+        except User.DoesNotExist:
+            return Response({"error": "Usuário não encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+        enterprise = self.get_enterprise_user(user.id)  # Corrigindo chamada de função
+
         serializer = UserSerializer(user)
-    
+
         return Response({
             "user": serializer.data,
             "enterprise": enterprise
-        })
-    
-    
+        }, status=status.HTTP_200_OK)
